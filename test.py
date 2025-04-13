a57055610e6dad068ed955966ac4486758393f86
Sheetal <sheetalbajajgodda@gmail.com>
2025-04-13 21:53:23 +0530

    prev

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 client = OpenAI(
     base_url="https://api.studio.nebius.com/v1/",
-    api_key="eyJhbGciOiJIUzI1NiIsImtpZCI6IlV6SXJWd1h0dnprLVRvdzlLZWstc0M1akptWXBvX1VaVkxUZlpnMDRlOFUiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDExMzQ1NjgyNDU1OTM5NDE0MTA3OSIsInNjb3BlIjoib3BlbmlkIG9mZmxpbmVfYWNjZXNzIiwiaXNzIjoiYXBpX2tleV9pc3N1ZXIiLCJhdWQiOlsiaHR0cHM6Ly9uZWJpdXMtaW5mZXJlbmNlLmV1LmF1dGgwLmNvbS9hcGkvdjIvIl0sImV4cCI6MTkwMjIzODk4NiwidXVpZCI6IjcwN2VlMTBhLWU3ZjktNGQ3ZS1hYTg3LTRjOTY3YzRjYzI5NiIsIm5hbWUiOiJBc3luYyBkZW1vIiwiZXhwaXJlc19hdCI6IjIwMzAtMDQtMTJUMTU6NDM6MDYrMDAwMCJ9.S3tS2c4luF6UYViiBFsGj60jKT1fAtgHtm74YJYmv-o"
+    api_key=os.environ.get("NEBIUS_API_KEY")
 )
 
 response = client.chat.completions.create(
